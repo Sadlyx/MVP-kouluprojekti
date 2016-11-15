d11176fe8447a2b95ad979d057575a00dce433a1
Sadlyx <sad_li@live.fi>
2016-11-15 07:52:45 +0200

    Updated sivusto palvelimelta

---
+++ conf.php
@@ -0,0 +1,7 @@
+<?php
+$conf = [];
+$conf['sivut'] = [  "etusivu" => "etusivu.php",
+			"tekijat" => "tekijat.php",
+			"it-tehtavat" => "it-tehtavat.php",	
+		];
+?>
--- js/_scripts.js
+++ js/_scripts.js
@@ -1,8 +1,42 @@
+$.ajaxSetup ({
+    // Disable caching of AJAX responses
+    cache: false
+});
 $("#mobile_menu_btn").click(function(){
 	$("#menu").stop().dequeue().fadeToggle(250); 
+	$("#engmenu").stop().dequeue().fadeToggle(250); 
 	/*if($("#menu").css("display") == "none"){
 		$("#menu").show(500);
 	} else {
 		$("#menu").hide(500);
 	}*/
 });
+	$("#menu").load("../_nav.html"); 
+	$("#engmenu").load("../engnav.html"); 
+	  
+$( document ).ready(function() {
+	$(".d_btn").mouseenter(function(){
+		$("ul").find("[data-parent='" + $(this).html() + "']").fadeIn(250);
+			
+		});
+	$(".d_btn").click(function(){
+		if($("ul").find("[data-parent='" + $(this).html() + "']").css("opacity") == 1 && $("ul").find("[data-parent='" + $(this).html() + "']").css("display") != "none"){
+			$("ul").find("[data-parent='" + $(this).html() + "']").fadeOut(250);
+		} else{
+		$("ul").find("[data-parent='" + $(this).html() + "']").fadeIn(250);
+		} 
+	});
+	$(".dropdown").mouseleave(function(){
+		$(this).fadeOut(250);
+	});
+});
+
+
+
+
+
+/*SHIV*/
+/**
+* @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
+*/
+!function(a,b){function c(a,b){var c=a.createElement("p"),d=a.getElementsByTagName("head")[0]||a.documentElement;return c.innerHTML="x<style>"+b+"</style>",d.insertBefore(c.lastChild,d.firstChild)}function d(){var a=t.elements;return"string"==typeof a?a.split(" "):a}function e(a,b){var c=t.elements;"string"!=typeof c&&(c=c.join(" ")),"string"!=typeof a&&(a=a.join(" ")),t.elements=c+" "+a,j(b)}function f(a){var b=s[a[q]];return b||(b={},r++,a[q]=r,s[r]=b),b}function g(a,c,d){if(c||(c=b),l)return c.createElement(a);d||(d=f(c));var e;return e=d.cache[a]?d.cache[a].cloneNode():p.test(a)?(d.cache[a]=d.createElem(a)).cloneNode():d.createElem(a),!e.canHaveChildren||o.test(a)||e.tagUrn?e:d.frag.appendChild(e)}function h(a,c){if(a||(a=b),l)return a.createDocumentFragment();c=c||f(a);for(var e=c.frag.cloneNode(),g=0,h=d(),i=h.length;i>g;g++)e.createElement(h[g]);return e}function i(a,b){b.cache||(b.cache={},b.createElem=a.createElement,b.createFrag=a.createDocumentFragment,b.frag=b.createFrag()),a.createElement=function(c){return t.shivMethods?g(c,a,b):b.createElem(c)},a.createDocumentFragment=Function("h,f","return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&("+d().join().replace(/[\w\-:]+/g,function(a){return b.createElem(a),b.frag.createElement(a),'c("'+a+'")'})+");return n}")(t,b.frag)}function j(a){a||(a=b);var d=f(a);return!t.shivCSS||k||d.hasCSS||(d.hasCSS=!!c(a,"article,aside,dialog,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}mark{background:#FF0;color:#000}template{display:none}")),l||i(a,d),a}var k,l,m="3.7.3",n=a.html5||{},o=/^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i,p=/^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i,q="_html5shiv",r=0,s={};!function(){try{var a=b.createElement("a");a.innerHTML="<xyz></xyz>",k="hidden"in a,l=1==a.childNodes.length||function(){b.createElement("a");var a=b.createDocumentFragment();return"undefined"==typeof a.cloneNode||"undefined"==typeof a.createDocumentFragment||"undefined"==typeof a.createElement}()}catch(c){k=!0,l=!0}}();var t={elements:n.elements||"abbr article aside audio bdi canvas data datalist details dialog figcaption figure footer header hgroup main mark meter nav output picture progress section summary template time video",version:m,shivCSS:n.shivCSS!==!1,supportsUnknownElements:l,shivMethods:n.shivMethods!==!1,type:"default",shivDocument:j,createElement:g,createDocumentFragment:h,addElements:e};a.html5=t,j(b),"object"==typeof module&&module.exports&&(module.exports=t)}("undefined"!=typeof window?window:this,document);
